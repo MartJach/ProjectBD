871493a40fba1544a67be94e6389bf9510fbc124
lukasz2017 <djokus@o2.pl>
2018-01-07 16:51:56 +0000

    Add logo, change texts

--- front-end-app/src/views/Login/index.js
+++ front-end-app/src/views/Login/index.js
@@ -2,18 +2,33 @@ import React from 'react';
 import { Container, Row, Col, CardGroup, Card, CardBody, Button, Input, InputGroup, InputGroupAddon } from 'reactstrap';
 import LoginAuth from '../../components/LoginAuth/LoginAuth';
 import '../../../scss/views/style.scss';
+import Logo from '../../../public/img/logo.png';
+
+const styles = {
+  card: {
+    backgroundColor: 'transparent',
+    border: 'none',
+  },
+  container: {
+    background: 'radial-gradient(ellipse at center, rgba(125,185,232,1) 0%,rgba(30,87,153,1) 100%);',
+  },
+  logoPosition: {
+    display: 'block',
+    margin: '0 auto 100px auto',
+  },
+};
 
 const Login = () => (
   <div className="animated fadeIn flex-row align-items-center">
-    <div className="app flex-row align-items-center">
+    <div className="app flex-row align-items-center" style={styles.container}>
       <Container>
         <Row className="justify-content-center">
           <Col md="6">
             <CardGroup className="mb-0">
-              <Card className="p-4">
+              <Card className="p-4" style={styles.card}>
                 <CardBody className="card-body">
-                  <h1>eHealth - admin </h1>
-                  <p className="text-muted">Sign In to your admin panel</p>
+                  <img src={Logo} style={styles.logoPosition} />
+                  <p className="text-muted">Zaloguj się do panelu administracyjnego</p>
                   <LoginAuth />
                 </CardBody>
               </Card>
